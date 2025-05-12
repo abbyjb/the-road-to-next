9c1d6e17f6a96d0812fb5dcd0e4ff07ab419acdc
Abby Bashore <abbyjbdev@gmail.com>
2025-05-12 08:56:54 -0700

    typescript section
    - tsc
    - const assertions

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X"
+}
+
 const TicketsPage = () => {
   return (
     <div>
@@ -9,6 +15,7 @@ const TicketsPage = () => {
 
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-3xl">{ticket.title}</h2>
 
           <Link href={ticketPath(ticket.id)} className="text-lg underline">
