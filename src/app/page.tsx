88c0c4634c8782d06ac6ce504a8eddc13a7641ff
Abby Bashore <abbyjbdev@gmail.com>
2025-05-14 07:35:57 -0700

    tailwind section
    - general tailwind styles
    - conditional styling with clsx
    - animations
    - svgs for ticket statuses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,20 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-6xl">Hello Page</h2>
-
-      <Link href={ticketsPath()} className="text-lg underline">
-        Go to Tickets
-      </Link>
+    <div className="flex flex-1 flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">
+          Hello Page
+        </h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+      <div className="flex flex-1 flex-col items-center">
+        <Link href={ticketsPath()} className="text-lg underline">
+            Go to Tickets
+        </Link>
+      </div>
     </div>
   )
 }
